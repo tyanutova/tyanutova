48bf45fc70ac2e40402978370ef9e4c41ac03300
Анна <miskuzi.madam@icloud.com>
2024-04-19 11:28:29 +0300

    /

--- lab7.1.py
+++ lab7.1.py
@@ -1,8 +1,7 @@
 from PIL import Image
 
-image_path = "C:/Users/Анна/Desktop/amnyam.jpeg"
+image_path = "C:/Users/Вероника/Desktop/z1.jpeg"
 image = Image.open(image_path)
-
 width, height = image.size
 print("Размер изображения: {}x{}".format(width, height))
 print("Формат изображения: {}".format(image.format))
--- lab7.2.py
+++ lab7.2.py
@@ -1,16 +1,12 @@
 from PIL import Image
 
-image = Image.open("C:/Users/Анна/Desktop/amnyam.jpeg")
-
+image = Image.open("C:/Users/Вероника/Desktop/z1.jpeg")
 width, height = image.size
-
 small_image = image.resize((width // 3, height // 3))
-small_image.save("small_" + "amnyam.jpeg")
-
+small_image.save("small_" + "1.jpeg")
 horizontal_mirror = image.transpose(Image.FLIP_LEFT_RIGHT)
-horizontal_mirror.save("horizontal_mirror_" + "amnyam.jpeg")
-
+horizontal_mirror.save("horizontal_mirror_" + "z1.jpeg")
 vertical_mirror = image.transpose(Image.FLIP_TOP_BOTTOM)
-vertical_mirror.save("vertical_mirror_" + "amnyam.jpeg")
+vertical_mirror.save("vertical_mirror_" + "z1.jpeg")
 
-print("Изображения успешно сохранены!")
+print("Изображения сохранены!")
