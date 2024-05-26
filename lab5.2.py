b84bd9e57635411fc3a993a8d5d288c1eba6ea69
Анна <miskuzi.madam@icloud.com>
2024-05-26 19:28:00 +0300

    /

--- lab5.2.py
+++ lab5.2.py
@@ -1,15 +1,11 @@
-my_list = [3, 6, 8, 3, 2, 9, 6, 3]
+import random
+def ex52():
 
-def find_duplicates(lst):
-    seen = set()
-    duplicates = set(x for x in lst if x in seen or seen.add(x))
-    return list(duplicates)
-
-duplicates = find_duplicates(my_list)
-
-if duplicates:
-    print("Повторяющиеся элементы в списке:")
-    for duplicate in duplicates:
-        print(duplicate)
-else:
-    print("В списке нет повторяющихся элементов.")
+    newOne = []
+    spisok = [random.randint(1, 10) for x in range(0, 50)]
+    for i in range(len(spisok)):
+        if (spisok[i] in spisok[i+1:]) and spisok[i] not in newOne:
+            newOne.append(spisok[i])
+    print(newOne)
+    print(spisok)
+ex52()
